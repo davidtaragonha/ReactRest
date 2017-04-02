32bec526c6e309f15a532e011c4cf642eacf21b1
David Alonso Tubio <davidtaragonha@gmail.com>
2017-04-02 19:48:20 +0200

    Eject from create react app all dependencies

--- src/routes/configureRoutes.tsx
+++ src/routes/configureRoutes.tsx
@@ -1,15 +1,27 @@
 import * as React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, RouterState, RedirectFunction } from 'react-router';
 import App from '../pages/App';
 import Login from '../pages/Login';
 import Main from '../pages/Main';
 import Specimens from '../pages/Specimens';
 import Groups from '../pages/Groups';
 
+function requireAuth(nextState: RouterState, replace: RedirectFunction) {
+  console.log(nextState);
+  if (true) {
+    /*
+    replace({
+      pathname: '/',
+      state: { nextPathname: nextState.location.pathname }
+    })
+    */
+  }
+}
+
 export default (
     <Route path="/" component={App}>
       <IndexRoute component={Login} />
-      <Route path="main" component={Main}>
+      <Route path="main" component={Main} onEnter={requireAuth}>
         <Route path="specimens" component={Specimens} />
         <Route path="groups" component={Groups} />
       </Route>
